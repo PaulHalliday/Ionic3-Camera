7c9ac0df7729782ce8facb6cfa378687584fc3ef
Paul Halliday <Paul_DEV@pauls-imac.home>
2017-06-06 11:25:29 +0100

    Ionic 3 Camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from "@ionic-native/camera";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,49 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  image: string;
 
+  constructor(private camera: Camera) {}
+
+  async pictureFromCamera() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      saveToPhotoAlbum: true
+    }
+
+    this.capturePhoto(options);
+  }
+
+  pictureFromGallery() {
+    //Added PictureSourceType.PHOTOLIBRARY to access from gallery.
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      saveToPhotoAlbum: true
+    }
+
+    this.capturePhoto(options);
+  }
+
+  async capturePhoto(options: CameraOptions) {
+    try {
+      //Result is a base64 image but can be changed to use a filepath.
+      const result = await this.camera.getPicture(options)
+
+      //Append result to image to display in view
+      this.image = `data:image/jpeg;base64,${result}`;
+    }
+    catch (e) {
+      console.error(e);
+    }
   }
 
 }
